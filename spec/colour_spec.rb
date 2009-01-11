64fc5a6950ddc73c633760e5964b2463005f0917
Wes Devauld <wes@devauld.ca>
2009-01-11 12:04:14 -0700

    Cleanup Bones, stub out specifications

--- spec/colour_spec.rb
+++ spec/colour_spec.rb
@@ -2,7 +2,36 @@
 
 require File.join(File.dirname(__FILE__), %w[spec_helper])
 
-describe Colour do
+describe "every colour", :shared => true do
+   it "should convert to RGB" do
+   end
+
+   it "should convert to HSV" do
+   end
+
+   it "should convert to CMYK" do
+   end
+
+   it "should provide a Web Safe Hex" do
+   end
+
+   it "should provide a complementary colour" do
+   end
+
+   it "should provide a split complemenatry set of colours" do
+   end
+
+   it "should apply a block when generating a split complementary set of colours if one is provided" do
+   end
+
+   it "should provide a series of colours rotating around the Hue wheel" do
+   end
+
+   it "should apply a block to the colours when generating the colours around the colour wheel if one is provided" do
+   end
+
+   it "should generate triadic colours" do
+   end
 end
 
 # EOF
